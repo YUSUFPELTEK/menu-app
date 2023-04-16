1f5c6cdd8ed6f73f2c8e1fcb2e3f8de48e7fe865
YUSUFPELTEK <yusufpeltek0@gmail.com>
2023-04-16 16:00:31 +0300

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by YUSUF on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
